16db60b9495f384579a96b8ebd9f38a10bb32f2e
Jan Paul <jan.paul@dfki.de>
2015-11-30 17:50:48 +0100

    Data for Space Mouse state taken from old interaction types Pose and Velocity6D

--- interfaces/src/InteractionStates/SpaceMouse.h
+++ interfaces/src/InteractionStates/SpaceMouse.h
@@ -6,15 +6,20 @@
  */
 
 
-#include <envire_core/items/Frame.hpp>
+//#include <envire_core/items/Frame.hpp>
 
 #ifndef SIMULATION_MARS_INTERFACES_SRC_INTERACTIONSTATES_SPACEMOUSE_H_
 #define SIMULATION_MARS_INTERFACES_SRC_INTERACTIONSTATES_SPACEMOUSE_H_
 
 class SpaceMouse {
 public:
-	SpaceMouse();
+	SpaceMouse(const std::string& name) : name_(name) {}; //In case we want two or more Space Mice, give it a unique name in the Envire2 graph
+	SpaceMouse() : name_("SpaceMouse") {};
 	virtual ~SpaceMouse();
+private:
+	mars::interaction::datatype::Pose poseState_;
+	mars::interaction::datatype::Velocity6D velocityState_;
+	std::string name_="SpaceMouse";
 };
 
 #endif /* SIMULATION_MARS_INTERFACES_SRC_INTERACTIONSTATES_SPACEMOUSE_H_ */
